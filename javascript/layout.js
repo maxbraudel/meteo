2b37c452c44ac5d15128f74e46cc51838383c90a
Max Braudel <dev.maxbraudel@gmail.com>
2024-12-20 05:07:50 +0100

    harmonisation  des fichiers html, layout css, amélioration responsiveness index, ajout font, amélioration structure projet

---
+++ javascript/layout.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // NAVIGATION MENU
+
+    const navBar = document.getElementById('navBar');
+    const menuIcon = document.getElementById('mobile-menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const returnButton = document.getElementById('return');
+
+    // Ouverture du menu mobile
+    menuIcon.addEventListener('click', () => {
+        mobileMenu.style.display = 'flex';
+        navBar.style.display = 'none'
+    });
+
+    // Fermeture du menu mobile
+    returnButton.addEventListener('click', () => {
+        mobileMenu.style.display = 'none';
+        navBar.style.display = 'flex'
+    });
+
+})
