cf9836fa1344d7a68c9d3aa5e0229379ec61064b
mariblnd <marieblanchard7@laposte.net>
2024-12-22 12:05:50 +0100

    ajout d'attributs aria

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+//Supprimer le filigrane
+const loopToRemoveWatermark = setInterval(() => {
+    
+    if (document.querySelector('spline-viewer')) {
+
+        const customTag = document.querySelector('spline-viewer');
+        const shadowRoot = customTag.shadowRoot;
+
+        const logo = shadowRoot.querySelector('#logo');
+        if (logo) {
+            logo.remove();
+            clearInterval(loopToRemoveWatermark);
+        }
+
+    }
+
+}, 1);
